f228ef6df026491b90f81620603c96e2c2c43812
AndrewMpel <dit22138@office365.uop.gr>
2025-06-15 18:31:03 +0300

    Application message responsive update

--- frontend/html/NavBar.php
+++ frontend/html/NavBar.php
@@ -1,11 +1,10 @@
 <?php
-
 $sql = "SELECT start, end FROM application_period ORDER BY start DESC LIMIT 1";
 $result = $conn->query($sql);
 
 $canApply = false;
-$application_message = ""; 
-$display_date_range_for_active_period = ""; 
+$application_message = "";
+$display_date_range_for_active_period = "";
 
 if ($result && $result->num_rows > 0) {
     $row = $result->fetch_assoc();
@@ -35,6 +34,7 @@ if ($conn) {
 
 <div class="Navbar">
     <?php
+    include '../../backend/db.php';
     $final_message_to_display = "";
     if ($canApply) {
         $final_message_to_display = $display_date_range_for_active_period;
@@ -44,22 +44,19 @@ if ($conn) {
 
     if (!empty($final_message_to_display)):
     ?>
-    
+
     <?php endif; ?>
 
     <button class="Navbuttons"><a href="../html/index.php" class="NavLinks">Αρχική</a></button>
     <button class="Navbuttons"><a href="../html/reqs.html" class="NavLinks">Απαιτήσεις</a></button>
-
     <?php if ($canApply): ?>
         <button class="Navbuttons"><a href="../html/application.php" class="NavLinks">Αίτηση</a></button>
     <?php else: ?>
         <button class="Navbuttons" disabled style="opacity: 0.5; cursor: not-allowed;">Αίτηση</button>
     <?php endif; ?>
+    <button class="Navbuttons"><a href="../html/more.html" class="NavLinks">Περισσότερα</a></button>
 
-   
     <?php
-    
-    include '../../backend/db.php';
     $sql_admin = "SELECT * FROM users WHERE is_admin = 1";
     $result_admin = $conn->query($sql_admin);
     if ($result_admin && $result_admin->num_rows > 0) {
@@ -75,9 +72,7 @@ if ($conn) {
         $conn->close();
     }
     ?>
-
- <button class="Navbuttons"><a href="../html/more.html" class="NavLinks">Περισσότερα</a></button>
-<div style="background-color: #ffeeee; color: red; padding: 10px; border: 1px solid red; text-align: center; margin-bottom: 20px;">
+    <div class="application-status-message-container">
         <?php echo $final_message_to_display; ?>
     </div>
 
